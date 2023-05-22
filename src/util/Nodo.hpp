719e7dfa28fb3176565f5db528a57a04ef7e5149
Martina Juri <martina.juri@mi.unc.edu.ar>
2023-05-22 15:38:17 -0300

    utils

---
+++ src/util/Nodo.hpp
@@ -0,0 +1,37 @@
+#include <cstdlib>
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+#ifndef NODO_HPP
+#define NODO_HPP
+template <class T>
+class Nodo
+{
+private:
+    T *dato;        // Puntero al dato del nodo
+    Nodo *next;     // Puntero al siguiente nodo
+public:
+    Nodo() {
+        next = NULL;
+        dato = NULL;
+    };
+    
+    Nodo(T *a){
+        dato = a;
+        next = NULL;
+    };
+    
+    //setters       
+    void setDato(T *a) { dato = a; };    
+    void setNext(Nodo *n) { next = n; }; 
+
+    //getters
+    T *getDato() { return dato; };       
+    Nodo *getNext() { return next; };
+
+    //devuelve si el nodo no tiene siguiente nodo apuntado
+    bool isEmpty() { return next == NULL; }
+};
+#endif
---
+++ src/util/Queue.hpp
@@ -0,0 +1,22 @@
+#include <cstdlib>
+#include <iostream>
+#include <sstream>
+#include "List.hpp"
+#include "List.cpp"
+
+using namespace std;
+
+#ifndef QUEUE_HPP
+#define QUEUE_HPP
+template <class T> class Queue :public List<T> {
+public:
+    Queue() { List<T>(); };
+    T* tope() { return this->last(); };
+    bool colavacia(void) { return this->isEmpty(); };
+    void encolar(T *a) { this->add(a); };
+    void desencolar(void) { this->borrar_ultimo(); };
+    T* ultimo(void) { return this->cabeza(); };
+    //string toPrint(string s) { return this->toPrint(s); };
+    void imprimirQueue(){cout<< " Cola de pedidos: " << endl; this->imprimirQ();}
+};
+#endif
