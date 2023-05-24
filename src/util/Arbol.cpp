3af57925aae01a37c108b132cf03242baf750725
Martina Juri <martina.juri@mi.unc.edu.ar>
2023-05-24 17:02:22 -0300

    new arbol

---
+++ src/util/Arbol.cpp
@@ -0,0 +1,54 @@
+#include <cstdlib>
+#include <iostream>
+#include <sstream>
+#include "Arbol.hpp"
+
+using namespace std;
+
+template <class T> void Arbol<T>::CreaArbolBus(T x)
+{
+    ArbolBusq(x, raiz);
+}
+template <class T> void Arbol<T>::ArbolBusq(T x, nodo<T>*& nuevo)
+{
+    if (nuevo == NULL) {
+        nuevo = new nodo<T>;
+        nuevo->info = x; nuevo->der = nuevo->izq = NULL;
+    }
+    if (x > nuevo->info) ArbolBusq(x, nuevo->der);
+    if (x < nuevo->info) ArbolBusq(x, nuevo->izq);
+}
+template <class T> void Arbol<T>::ird(nodo<T>* aux)
+{
+    if (aux != NULL) {
+        ird(aux->izq);
+        cout << "\n" << aux->info;
+        ird(aux->der);
+    }
+}
+template <class T> void Arbol<T>::rid(nodo<T>* aux)
+{
+    if (aux != NULL) {
+        cout << "\n" << aux->info;
+        rid(aux->izq);
+        rid(aux->der);
+    }
+}
+template <class T> void Arbol<T>::idr(nodo<T>* aux)
+{
+    if (aux != NULL) {
+        idr(aux->izq);
+        idr(aux->der);
+        cout << "\n" << aux->info;
+    }
+}
+template <class T> void Arbol<T>::show(nodo<T>* aux, int n)
+{
+    int i;
+    if (aux != NULL) {                      //OjO este es un recorrido dri
+        show(aux->der, n + 1);
+        for (i = 1; i <= n; i++) cout << "     ";
+        cout << aux->info << "\n";
+        show(aux->izq, n + 1);
+    }
+}
